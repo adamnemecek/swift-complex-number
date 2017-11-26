27ddf44f0d3c9aaf9536c56a3b44d5942106456f
Adam Nemecek <adamnemecek@gmail.com>
2017-11-26 13:09:22 -0800

    * c1 -> lhs, c2 -> rhs

--- ComplexNumber.playground/Sources/ComplexOperator.swift
+++ ComplexNumber.playground/Sources/ComplexOperator.swift
@@ -13,33 +13,33 @@ infix operator ∠ : AdditionPrecedence
 
 extension Complex {
 
-    public static func + (c1: Complex, c2: Complex) -> Complex { return c1.add(c2) }
-    public static func + (c1: Double,  c2: Complex) -> Complex { return Complex(real: c1).add(c2) }
-    public static func + (c1: Complex, c2: Double ) -> Complex { return c1.add(Complex(real: c2)) }
+    public static func + (lhs: Complex, rhs: Complex) -> Complex { return lhs.add(rhs) }
+    public static func + (lhs: Double,  rhs: Complex) -> Complex { return Complex(real: lhs).add(rhs) }
+    public static func + (lhs: Complex, rhs: Double ) -> Complex { return lhs.add(Complex(real: rhs)) }
 
-    public static func - (c1: Complex, c2: Complex) -> Complex { return c1.subtract(c2) }
-    public static func - (c1: Double,  c2: Complex) -> Complex { return Complex(real: c1).subtract(c2) }
-    public static func - (c1: Complex, c2: Double ) -> Complex { return c1.subtract(Complex(real: c2)) }
+    public static func - (lhs: Complex, rhs: Complex) -> Complex { return lhs.subtract(rhs) }
+    public static func - (lhs: Double,  rhs: Complex) -> Complex { return Complex(real: lhs).subtract(rhs) }
+    public static func - (lhs: Complex, rhs: Double ) -> Complex { return lhs.subtract(Complex(real: rhs)) }
 
-    public static func * (c1: Complex, c2: Complex) -> Complex { return c1.multiply(c2) }
-    public static func * (c1: Double,  c2: Complex) -> Complex { return c2.multiply(c1) }
-    public static func * (c1: Complex, c2: Double ) -> Complex { return c1.multiply(c2) }
+    public static func * (lhs: Complex, rhs: Complex) -> Complex { return lhs.multiply(rhs) }
+    public static func * (lhs: Double,  rhs: Complex) -> Complex { return rhs.multiply(lhs) }
+    public static func * (lhs: Complex, rhs: Double ) -> Complex { return lhs.multiply(rhs) }
 
-    public static func / (c1: Complex, c2: Complex) -> Complex { return c1.divide(c2) }
-    public static func / (c1: Double,  c2: Complex) -> Complex { return Complex(real: c1).divide(c2) }
-    public static func / (c1: Complex, c2: Double ) -> Complex { return c1.divide(c2) }
+    public static func / (lhs: Complex, rhs: Complex) -> Complex { return lhs.divide(rhs) }
+    public static func / (lhs: Double,  rhs: Complex) -> Complex { return Complex(real: lhs).divide(rhs) }
+    public static func / (lhs: Complex, rhs: Double ) -> Complex { return lhs.divide(rhs) }
 
-    public static func ^ (c1: Complex, n: Double) -> Complex { return c1.power(n) }
-    public static func ^ (c1: Complex, n: Int) -> Complex { return c1.power(n) }
+    public static func ^ (lhs: Complex, n: Double) -> Complex { return lhs.power(n) }
+    public static func ^ (lhs: Complex, n: Int) -> Complex { return lhs.power(n) }
 
-    public static func += (c1: inout Complex, c2: Complex) { c1.addInPlace(c2) }
-    public static func += (c1: inout Complex, c2: Double) { c1.addInPlace(Complex(real: c2)) }
-    public static func -= (c1: inout Complex, c2: Complex) { c1.subtractInPlace(c2) }
-    public static func -= (c1: inout Complex, c2: Double) { c1.subtractInPlace(Complex(real: c2)) }
-    public static func *= (c1: inout Complex, c2: Complex) { c1.multiplyInPlace(c2) }
-    public static func *= (c1: inout Complex, c2: Double) { c1.multiplyInPlace(c2) }
-    public static func /= (c1: inout Complex, c2: Complex) { c1.divideInPlace(c2) }
-    public static func /= (c1: inout Complex, c2: Double) { c1.divideInPlace(c2) }
+    public static func += (lhs: inout Complex, rhs: Complex) { lhs.addInPlace(rhs) }
+    public static func += (lhs: inout Complex, rhs: Double) { lhs.addInPlace(Complex(real: rhs)) }
+    public static func -= (lhs: inout Complex, rhs: Complex) { lhs.subtractInPlace(rhs) }
+    public static func -= (lhs: inout Complex, rhs: Double) { lhs.subtractInPlace(Complex(real: rhs)) }
+    public static func *= (lhs: inout Complex, rhs: Complex) { lhs.multiplyInPlace(rhs) }
+    public static func *= (lhs: inout Complex, rhs: Double) { lhs.multiplyInPlace(rhs) }
+    public static func /= (lhs: inout Complex, rhs: Complex) { lhs.divideInPlace(rhs) }
+    public static func /= (lhs: inout Complex, rhs: Double) { lhs.divideInPlace(rhs) }
 }
 
 public prefix func ∠ (arg: Double) -> Complex { return cos(arg) + sin(arg) * 𝒊 }
